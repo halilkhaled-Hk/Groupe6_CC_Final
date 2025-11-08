e5d8d4a0ccaa6037b0636c78248864589b8bf4f5
Alex-Noa55 <noafranck04@gmail.com>
2025-11-08 14:42:50 +0100

    Feat: Add GET /projects/:id endpoint (fixes #3)

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from routes import post_project
+from routes import post_project, get_project_by_id
 
 app = FastAPI(title="ProjetAPI - TP GitHub DevOps")
 
 # Inclusion des routes
 app.include_router(post_project.router)
-
+app.include_router(get_project_by_id.router)
 
 @app.get("/")
 def root():
