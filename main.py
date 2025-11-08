3a03e50fb61cac74517eafe13375cc94983b554b
nell852 <nyogognell@gmail.com>
2025-11-08 18:39:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from routes import post_project
-from routes import get_projects
+
 app = FastAPI(title="ProjetAPI - TP GitHub DevOps")
 
 # Inclusion des routes
 app.include_router(post_project.router)
-app.include_router(get_projects.router)
+
 
 @app.get("/")
 def root():
