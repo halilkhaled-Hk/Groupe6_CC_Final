18a3aef4db0f015cb344809e98c84080a181b06d
Abdoul <presi7030@gmail.com>
2025-11-08 17:06:44 +0100

    correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routes import post_project
-from routes import Get_Project
+from routes import get_projects
 app = FastAPI(title="ProjetAPI - TP GitHub DevOps")
 
 # Inclusion des routes
