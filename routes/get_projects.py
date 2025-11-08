be303a159822825aedba64d8be8b17eff2554f6f
Abdoul <presi7030@gmail.com>
2025-11-08 16:34:26 +0100

    Ajout du code

---
+++ routes/get_projects.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, HTTPException
+import json
+
+router = APIRouter()
+
+DB_PATH = "db.json"
+
+@router.get("/projects")
+def get_all_projects():
+    try:
+        with open(DB_PATH, "r") as f:
+            data = json.load(f)
+        return data
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Fichier db.json introuvable")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
