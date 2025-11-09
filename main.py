a7bcbf33b7b30a2619323a92d8d970ef948ffa46
nell852 <nyogognell@gmail.com>
2025-11-09 09:27:05 +0100

    Mise à jour de ma branche

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from routes import post_project
+from routes import get_projects
+from routes import get_project_by_id
 
 app = FastAPI(title="ProjetAPI - TP GitHub DevOps")
 
 # Inclusion des routes
 app.include_router(post_project.router)
+app.include_router(get_projects.router)
+app.include_router(get_project_by_id.router)
 
 
 @app.get("/")
