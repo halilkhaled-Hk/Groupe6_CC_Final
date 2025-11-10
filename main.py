4fd8092ba220850a494e605a52816c6e7cefbb1d
DeffoMyriam <deffom8@gmail.com>
2025-11-10 06:38:59 +0100

    fix: fichiers _pycache_ supprimés et conflits dans main.py de db.json resolus

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from routes import post_project
 from routes import get_projects
 from routes import get_project_by_id
 from routes import put_projects_id_grade
+from routes import get_projects_by_course
 
 app = FastAPI(title="ProjetAPI - TP GitHub DevOps")
 
@@ -11,6 +12,7 @@ app.include_router(post_project.router)
 app.include_router(get_projects.router)
 app.include_router(get_project_by_id.router)
 app.include_router(put_projects_id_grade.router)
+app.include_router(get_projects_by_course.router)
 
 
 @app.get("/")
